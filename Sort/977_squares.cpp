cdf7a86a98e8ad648109f59bb925110c42c403d4
xiao cheng <xcheng4@xiaos-MacBook-Pro.local>
2019-01-26 14:43:32 -0600

    array

---
+++ Sort/977_squares.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        int n = A.size(); 
+        vector<int> result(n, 0); 
+        
+        int i = 0, j = n-1; 
+        for(int p = n - 1; p >= 0; p--){
+            if(abs(A[i]) > abs(A[j])){
+                result[p] = A[i] * A[i]; 
+                i++;
+            }else{
+                result[p] = A[j] * A[j]; 
+                j--;
+            }
+        }
+        
+        return result;
+    }
+};
